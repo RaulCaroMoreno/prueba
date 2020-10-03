2d243aa10103eddf4a34208c8f7113b0f4b92b1f
raul973 <39560502+raul973@users.noreply.github.com>
2020-10-03 11:36:48 +0200

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Raúl
+ */
+public class Calculadora {
+    private int numIz;
+    private int numDer;
+    private char operacion;
+    
+    public void setnumIz(int n){
+        this.numIz=n;
+    }
+    public int getnumIz(){
+        return numIz;
+    }
+    public void setnumDer(int n){
+        this.numDer=n;
+    }
+    public int getNumDer(){
+        return numDer;
+    }
+    public void setOp(char op){
+        this.operacion=op;
+    }
+    public int calcular(){
+        int resul=0;
+        if(operacion=='+'){
+            resul=numIz+numDer;
+        }
+        if(operacion=='-'){
+            resul=numIz-numDer;
+        }
+        return resul;
+    }
+    
+    
+}
